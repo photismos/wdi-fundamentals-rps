4a6d7166b5dce478bbdceb75cdcd06b2d59c84a2
photismos <shuli@invisiblenetworks.co>
2015-08-29 19:35:57 -0400

    excercise 4.1 save

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -21,18 +21,25 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
+var move;
 function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    if (move == true) {
+        return move;
+    } else 
+    return getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    if (move == true) {
+        return move;
+    } else
+    return randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
